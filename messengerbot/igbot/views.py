2ebc0261281514e84342ccd8be2bb2fd1af9d812
iknowright <changchaishi@gmail.com>
2018-11-20 22:56:48 +0800

    can receive msg

--- messengerbot/igbot/views.py
+++ messengerbot/igbot/views.py
@@ -6,8 +6,38 @@ from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
+import requests
 
-# handle messages event
+post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % ACCESS_TOKEN
+
+# Handle messages events
+def handleMessage(sender_psid, received_message):
+    # Check if the message contains text
+    if received_message['text']:
+        # Create the payload for a basic text message
+        response = {
+            "text": "You sent the message: "+received_message["text"]+". Now send me an image!"
+        }
+        # Sends the response message
+        callSendAPI(sender_psid, response)    
+    return
+
+# Handles messaging_postbacks events
+def handlePostback(sender_psid, received_postback):
+    pass
+
+# Sends response messages via the Send API
+def callSendAPI(sender_psid, response):
+    # Construct the message body
+    print(response)
+    request_body = json.dumps({
+        'recipient': {
+        'id': sender_psid
+        },
+        'message': response
+    })
+    r = requests.post(post_url, headers={'Content-Type': 'application/json'}, data=request_body)
+    print(r.json())
 
 
 # Create your views here.
@@ -32,10 +62,27 @@ class IgBotView(generic.View):
             for entry in body['entry']:
                 # Get the webhook event. entry.messaging is an array, but 
                 # will only ever contain one event, so we get index 0
+
+                # Gets the body of the webhook event
                 webhook_event = entry['messaging'][0]
                 print(webhook_event)
-                sender_id = webhook_event['sender']['id']
-                message = webhook_event['message']
+
+                # Get the sender PSID
+                sender_psid = webhook_event['sender']['id']
+
+                if webhook_event.get('message'):
+                    print(webhook_event['message'])
+                    print(sender_psid)
+                    try:
+                        handleMessage(sender_psid, webhook_event['message'])
+                    except:
+                        return HttpResponse(status=404)
+                elif webhook_event.get('postback'):
+                    try:
+                        handlePostback(sender_psid, webhook_event['message'])
+                    except:
+                        return HttpResponse(status=404)
+                
             return HttpResponse(status=200)
         else :
             return HttpResponse(status=404)
