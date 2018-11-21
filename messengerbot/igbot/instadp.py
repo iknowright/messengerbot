d885939a39a108808207a2d9dbd6e8b2eb164596
iknowright <changchaishi@gmail.com>
2018-11-21 15:56:27 +0800

    insta ok

--- messengerbot/igbot/instadp.py
+++ messengerbot/igbot/instadp.py
@@ -10,17 +10,8 @@ import requests
 # spinnerFrames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
 
 from imgurpython import ImgurClient
+from igbot_setting import *
 import json
-
-# If you already have an access/refresh pair in hand
-client_id = '3c54f578350162a'
-client_secret = '540119cbb8118d81f4e6677162d47098d940e93d'
-access_token = '77189f5c5b15f963388b2a054c23ca38521e68ae'
-refresh_token = '166066836f65a4df97b922e29bdaaadcbb597e47'
-
-# Note since access tokens expire after an hour, only the refresh token is required (library handles autorefresh)
-client = ImgurClient(client_id, client_secret, access_token, refresh_token)
-
 
 def getID(username):
     url = "https://www.instagram.com/{}"
@@ -67,13 +58,9 @@ def main():
 
     r = requests.get(file_url, stream=True)
     if r.ok:
-        with open(fname, 'wb') as f:
-            n = requests.post("https://api.imgur.com/3/image", headers={"Authorization": "Bearer %s" % access_token}, data={"image":r.content})
-            response = n.json()
-            print(response)
-            
-
-            print("\033[92m✔ File save:\033[0m {}".format(fname))
+        n = requests.post("https://api.imgur.com/3/image", headers={"Authorization": "Bearer %s" % IMGUR_ACESS_TOKEN}, data={"image":r.content})
+        response = n.json()
+        print("\033[92m✔ Image save:\033[0m {}".format(response['data']['link']))
     else:
         print("Cannot make connection to download image")
 
