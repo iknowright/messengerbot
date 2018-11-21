a9d208022a23ed10291d22a41285a78a73d99bbd
iknowright <changchaishi@gmail.com>
2018-11-21 10:04:17 +0800

    testing quick reply

--- messengerbot/igbot/messageAPI.py
+++ messengerbot/igbot/messageAPI.py
@@ -10,3 +10,48 @@ class MessageAPI:
     def text_message(self, content):
         response_msg = json.dumps({"recipient": {"id": self.fb_id}, "message": {"text": content}})
         requests.post(post_url, headers={"Content-Type": "application/json"}, data=response_msg)
+
+    def image_message(self, content):
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Pictures",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": content,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+        response_msg = json.dumps({"recipient": {"id": self.fb_id}, "message": response})
+        requests.post(post_url, headers={"Content-Type": "application/json"}, data=response_msg)
+
+    def quickreply_message(self, content):
+        response = json.dumps({
+            "recipient":{
+                "id":self.fb_id
+            },
+            "message":{
+                "text": "Here is a quick reply!",
+                "quick_replies":[
+                    {
+                        "content_type":"location"
+                    }
+                ]
+            }  
+        })
+        requests.post(post_url, headers={"Content-Type": "application/json"}, data=response)
--- messengerbot/igbot/views.py
+++ messengerbot/igbot/views.py
@@ -53,25 +53,14 @@ def handleMessage(sender_psid, received_message):
     # Check if the message contains text
     if received_message.get('text'):
         # Create the payload for a basic text message
-        sender.text_message(received_message['text'])
+        # sender.text_message(received_message['text'])
+        sender.quickreply_message(received_message['text'])
         return
-    
+    elif received_message.get('attachments'):
+        # Get the URL of the message attachment
+        attachment_url = received_message['attachments'][0]['payload']['url']
+        sender.image_message(attachment_url)
     return
-
-
-# Sends response messages via the Send API
-# def callSendAPI(sender_psid, response):
-#     # Construct the message body
-#     request_body = json.dumps({
-#         'recipient': {
-#         'id': sender_psid
-#         },
-#         'message': response
-#     })
-#     r = requests.post(post_url, headers={'Content-Type': 'application/json'}, data=request_body)
-#     print("<-------------Start of Response------------->")
-#     print(r.json())
-#     print("<-------------End of Response------------->")
 
 
 # Create your views here.
@@ -96,26 +85,26 @@ class IgBotView(generic.View):
             for entry in body['entry']:
                 # Get the webhook event. entry.messaging is an array, but 
                 # will only ever contain one event, so we get index 0
+                print("<-------------Start of Githook Content------------->")
+                print(entry)
+                print("<-------------End of Githook Content------------->")
 
                 # Gets the body of the webhook event
-                webhook_event = entry['messaging'][0]
-                print("<-------------Start of Githook Content------------->")
-                print(webhook_event)
-                print("<-------------Start of Githook Content------------->")
+                if entry.get('messaging'):
+                    webhook_event = entry['messaging'][0]
+                    # Get the sender PSID
+                    sender_psid = webhook_event['sender']['id']
 
-                # Get the sender PSID
-                sender_psid = webhook_event['sender']['id']
-
-                if webhook_event.get('message'):
-                    try:
-                        handleMessage(sender_psid, webhook_event['message'])
-                    except:
-                        return HttpResponse(status=404)
-                elif webhook_event.get('postback'):
-                    try:
-                        handlePostback(sender_psid, webhook_event['postback'])
-                    except:
-                        return HttpResponse(status=404) 
-            return HttpResponse(status=200)
+                    if webhook_event.get('message'):
+                        try:
+                            handleMessage(sender_psid, webhook_event['message'])
+                        except:
+                            return HttpResponse()
+                    elif webhook_event.get('postback'):
+                        try:
+                            handlePostback(sender_psid, webhook_event['postback'])
+                        except:
+                            return HttpResponse()
+            return HttpResponse()
         else :
-            return HttpResponse(status=404)
+            return HttpResponse()
