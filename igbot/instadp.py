cd8930f6e5af9b7dc7b28f959281c325d8d0ee2d
iknowright <changchaishi@gmail.com>
2018-12-16 03:28:48 +0800

    this time i want to sleep so it work

--- igbot/instadp.py
+++ igbot/instadp.py
@@ -6,7 +6,7 @@ def getImageUrl(instagram_id):
     html = r.text
     img_url = re.findall('"profile_pic_url_hd":"(.*?)",', html)
     bio = re.findall('"biography":"(.*?)",', html)
-    if img_url is None:
+    if not img_url:
         return "", ""
     else :
         return img_url[0], bio[0]
